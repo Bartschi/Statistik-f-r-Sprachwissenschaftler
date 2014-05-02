62857560c6ddc68dd6084c3be20b7e7791e4a59e
Isabelle Bartsch <bartschi@students.uni-marburg.de>
2014-05-02 22:57:16 +0200

    Hausaufgabe05

--- Bartschi/CopyOfhausaufgabe05.R
+++ Bartschi/CopyOfhausaufgabe05.R
@@ -128,18 +128,18 @@ print(klinisch)
 # Jetzt brauchen wir die Teilmenge für die anderen beiden Studiengänge, 
 # Linguistik Kognition und Kommunikation und Speech Science
 # HINT: wie sehen die Namen aus bzw. wie werden sie im data frame buchstabiert?
-linkk <- frauen[frauen$major == "M.A..Linguistik Kognition und Kommunikation",]
-speech <- frauen[frauen$major == "M.A..Speech Science",] 
+linkk <- frauen[frauen$major == "M.A..Linguistik.Kognition.und.Kommunikation",]
+speech <- frauen[frauen$major == "M.A..Speech.Science",] 
 
 # Berechnen Sie -- ohne Hilfe von sd() -- die Standardabweichung für die Größe der drei 
 # Gruppen. Sie können auch weitere Zeilen hinzufügen, wenn es Ihnen so leichter
 # ist. 
 # HINT: Formel und Beispiel für die Berechnung auf den Folien!
-#klinisch.sd <- CODE_HIER
-#linkk.sd <- CODE_HIER
-#speech.sd <- CODE_HIER
+klinisch.sd <- sqrt( var(frauen$major== "M.A..Klinische.Linguistik",))
+linkk.sd <- sqrt( var(frauen$major== "M.A..Linguistik.Kognition.und.Kommunikation",))
+speech.sd <- sqrt( var(frauen$major== "M.A..Speech.Science",))
 
 # Berichten Sie jetzt die Mittelwerte und Standardabweichungen für die drei Gruppen. Die erste Gruppe steht hier als Muster:
-#print( paste("Studiengang: Klinische Linguistik","Mean:",mean(klinisch$height),"SD:",klinisch.sd) )
-#CODE_HIER
-
+print( paste("Studiengang: Klinische Linguistik","Mean:",mean(klinisch$height),"SD:",klinisch.sd) )
+print( paste("Studiengang: Linguistik Kogition und Kommunikation", "Mean:",mean(linkk$height),"SD:",linkk.sd))
+print( paste("Studiengang: Speech Science", "Mean:",mean(speech$height), "SD:",speech.sd))
